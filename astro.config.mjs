4ba155042b5fd5a85343a47d51991b902139ff4e
Tam <tambuildsthings@gmail.com>
2024-02-16 17:35:13 +0100

    gtm with proxies

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,26 @@ export default defineConfig({
   integrations: [
     tailwind(),
     partytown({
-      config: { forward: ["dataLayer.push"] },
+      config: {
+        // Allow proxying requests for Google domains
+        resolveUrl: (url, location) => {
+          const proxiedHosts = [
+            "googletagmanager.com",
+            "googleads.g.doubleclick.net",
+          ];
+
+          // Proxy any allowed hosts through the base domain
+          if (proxiedHosts.includes(url.hostname)) {
+            const proxyUrl = new URL(location.origin);
+            proxyUrl.searchParams.append("url", url.href);
+            return proxyUrl;
+          }
+
+          return url;
+        },
+        // Allow communication for Google Tag Manager
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
 });
